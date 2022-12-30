906649135d0ee5f8942e017483a6dab4f796840c
Marcus Rodan <marcus.rodan@sinch.com>
2022-12-30 13:34:01 +0100

    Add basic example

---
+++ main.go
@@ -0,0 +1,49 @@
+package pulumiplaygroundlib
+
+import (
+	"fmt"
+
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type SimplePublicS3FileShare struct {
+	pulumi.ResourceState
+	BucketName pulumi.IDOutput
+}
+
+func NewMyComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*SimplePublicS3FileShare, error) {
+	share := &SimplePublicS3FileShare{}
+
+	err := ctx.RegisterComponentResource("nerja:pulumi:SimplePublicS3FileShare", name, share, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	bucket, err := s3.NewBucket(ctx, fmt.Sprintf("%s-bucket", name), &s3.BucketArgs{
+		Website: s3.BucketWebsiteArgs{
+			IndexDocument: pulumi.String("index.html"),
+		},
+		Acl: pulumi.String("public-read"),
+	}, pulumi.Parent(share))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = s3.NewBucketObject(ctx, "obj", &s3.BucketObjectArgs{
+		Bucket:  bucket.ID(),
+		Content: pulumi.String("hello"),
+		Key:     pulumi.String("index.html"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	share.BucketName = bucket.ID()
+
+	ctx.RegisterResourceOutputs(share, pulumi.Map{
+		"bucketName": bucket.ID(),
+	})
+
+	return share, nil
+}
